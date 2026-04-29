a21f134a8165f0203e449e693d0588fc1f5dde8a
Fantamomo <git@fantamomo.com>
2026-04-29 17:37:34 +0200

    refactor(http): replace `ConditionalHeaders` with `CachingHeaders` in HTTP configuration

--- src/main/kotlin/com/fantamomo/server/amongus/app/HTTP.kt
+++ src/main/kotlin/com/fantamomo/server/amongus/app/HTTP.kt
@@ -1,12 +1,8 @@
 package com.fantamomo.server.amongus.app
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cachingheaders.*
 
 fun Application.configureHTTP() {
-//    install(ConditionalHeaders) {
-//        version { call, outgoingContent ->
-//            outgoingContent
-//            emptyList()
-//        }
-//    }
+    install(CachingHeaders)
 }
