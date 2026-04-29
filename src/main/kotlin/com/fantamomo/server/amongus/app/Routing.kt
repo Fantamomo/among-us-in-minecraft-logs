24aca1c4ec8c61e168c3b80c1ab6c2acc3f6ffb2
Fantamomo <git@fantamomo.com>
2026-04-29 17:37:56 +0200

    feat(routing): add caching headers and `/log/local` endpoint, enhance log handling
    - Introduce `CachingHeaders` for improved cache control on endpoints.
    - Add new `/log/local` endpoint for serving local logs.
    - Enhance `/log/{id}`, `/raw/{id}`, and new `HEAD` requests with cache options and response headers.
    - Refactor request parameter validation into a helper method to streamline log ID handling.
    - Support `ttl` parameter for log retention customization in `/upload` endpoint.

--- src/main/kotlin/com/fantamomo/server/amongus/app/Routing.kt
+++ src/main/kotlin/com/fantamomo/server/amongus/app/Routing.kt
@@ -1,17 +1,23 @@
 package com.fantamomo.server.amongus.app
 
 import com.fantamomo.server.amongus.LogManager
+import com.fantamomo.server.amongus.ServerConfig
 import com.fantamomo.server.amongus.ServerConstants
 import com.fantamomo.server.amongus.model.LogState
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.http.content.*
+import io.ktor.server.plugins.cachingheaders.*
 import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.*
+import kotlin.time.Clock
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRouting(manager: LogManager) {
     routing {
@@ -25,33 +31,67 @@ fun Application.configureRouting(manager: LogManager) {
             call.respondText(html, ContentType.Text.Html)
         }
         get("/raw/{id}") {
-            val id = call.parameters["id"] ?: throw IllegalArgumentException("Invalid ID")
+            val id = getId() ?: return@get
             val log = manager.getLog(id)
-            call.response.headers.append("AUIML", id)
             if (log != null && !log.isExpired() && log.lifecycle.state != LogState.ARCHIVED) {
+                call.caching = CachingOptions(CacheControl.MaxAge(60, visibility = CacheControl.Visibility.Private))
+                call.response.headers.append("auiml-log-code", id)
                 call.respond(HttpStatusCode.OK, log.readLog())
             } else {
                 call.respond(HttpStatusCode.NotFound)
             }
         }
-        get("/log/{id}") {
-            val id = call.parameters["id"] ?: throw IllegalArgumentException("Invalid ID")
+        get("/log/local") {
             val html = ServerConstants::class.java.getResourceAsStream("/asserts/log.html")?.reader()?.readText()
             if (html == null) {
-                call.respond(HttpStatusCode.NotFound)
+                call.respond(HttpStatusCode.InternalServerError)
                 return@get
             }
-            val injected = html.replace(
+            call.caching = CachingOptions(CacheControl.MaxAge(600, visibility = CacheControl.Visibility.Private))
+            call.response.headers.append("auiml-log-code", "local")
+            html.replace(
                 "window.__LOG_CODE__",
-                "window.__LOG_CODE__ = \"${id.replace("\"", "")}\""
+                "window.__LOG_CODE__ = 'local'"
             )
-            call.respondText(injected, ContentType.Text.Html)
+            call.respondText(html, ContentType.Text.Html)
+        }
+        head("/log/{id}") {
+            val id = getId() ?: return@head
+            call.caching = CachingOptions(CacheControl.MaxAge(600, visibility = CacheControl.Visibility.Private))
+            call.response.headers.append("auiml-log-code", id)
+            call.respond(HttpStatusCode.OK)
+        }
+        get("/log/{id}") {
+            val id = getId() ?: return@get
+            val html = ServerConstants::class.java.getResourceAsStream("/asserts/log.html")?.reader()?.readText()
+            if (html == null) {
+                call.respond(HttpStatusCode.InternalServerError)
+                return@get
+            }
+            call.caching = CachingOptions(CacheControl.MaxAge(600, visibility = CacheControl.Visibility.Private))
+            call.response.headers.append("auiml-log-code", id)
+            call.respondText(html, ContentType.Text.Html)
         }
         rateLimit {
             post("/upload") {
                 val log = call.receive<JsonObject>()
-                val id = manager.createLog(log)
-                call.respond(HttpStatusCode.Created, id)
+
+                val clientTtl = call.request.queryParameters["ttl"]
+                    ?.toIntOrNull()
+                    ?.seconds
+
+                val ttl = clientTtl?.takeIf { it in Duration.ZERO..ServerConfig.SAVE_LOG_DURATION }
+                    ?: ServerConfig.SAVE_LOG_DURATION
+
+                val id = manager.createLog(log, ttl)
+                val jsonObject = JsonObject(
+                    mapOf(
+                        "id" to JsonPrimitive(id),
+                        "ttl" to JsonPrimitive(ttl.inWholeSeconds),
+                        "deleteAt" to JsonPrimitive((Clock.System.now() + ttl).toEpochMilliseconds())
+                    )
+                )
+                call.respond(HttpStatusCode.Created, jsonObject)
             }
         }
         authenticate {
@@ -59,13 +99,17 @@ fun Application.configureRouting(manager: LogManager) {
                 route("/link") {
                     post {
                         val request = call.receive<JsonObject>()
-                        val logs = request["logs"]?.jsonArray?.map { it.jsonPrimitive.content } ?: throw IllegalArgumentException("Invalid ID")
+                        val logs = request["logs"]?.jsonArray?.map { it.jsonPrimitive.content }
+                            ?: throw IllegalArgumentException("Invalid ID")
                         if (logs.isEmpty()) {
                             call.respond(HttpStatusCode.BadRequest)
                         }
-                        val externalRepo = request["repo"]?.jsonPrimitive?.contentOrNull ?: throw IllegalArgumentException("Invalid repo")
-                        val externalId = request["id"]?.jsonPrimitive?.intOrNull ?: throw IllegalArgumentException("Invalid external ID")
-                        val externalType = request["type"]?.jsonPrimitive?.contentOrNull ?: throw IllegalArgumentException("Invalid external type")
+                        val externalRepo = request["repo"]?.jsonPrimitive?.contentOrNull
+                            ?: throw IllegalArgumentException("Invalid repo")
+                        val externalId = request["id"]?.jsonPrimitive?.intOrNull
+                            ?: throw IllegalArgumentException("Invalid external ID")
+                        val externalType = request["type"]?.jsonPrimitive?.contentOrNull
+                            ?: throw IllegalArgumentException("Invalid external type")
                         manager.removeLinks(externalRepo, externalId, externalType)
                         var success = false
                         for (log in logs) {
@@ -79,17 +123,23 @@ fun Application.configureRouting(manager: LogManager) {
                     }
                     post("/close") {
                         val request = call.receive<JsonObject>()
-                        val repo = request["repo"]?.jsonPrimitive?.contentOrNull ?: throw IllegalArgumentException("Invalid repo")
-                        val id = request["id"]?.jsonPrimitive?.intOrNull ?: throw IllegalArgumentException("Invalid external ID")
-                        val type = request["type"]?.jsonPrimitive?.contentOrNull ?: throw IllegalArgumentException("Invalid type")
+                        val repo = request["repo"]?.jsonPrimitive?.contentOrNull
+                            ?: throw IllegalArgumentException("Invalid repo")
+                        val id = request["id"]?.jsonPrimitive?.intOrNull
+                            ?: throw IllegalArgumentException("Invalid external ID")
+                        val type = request["type"]?.jsonPrimitive?.contentOrNull
+                            ?: throw IllegalArgumentException("Invalid type")
                         manager.linkClosed(repo, id, type)
                         call.respond(HttpStatusCode.OK)
                     }
                     post("/reopen") {
                         val request = call.receive<JsonObject>()
-                        val repo = request["repo"]?.jsonPrimitive?.contentOrNull ?: throw IllegalArgumentException("Invalid repo")
-                        val id = request["id"]?.jsonPrimitive?.intOrNull ?: throw IllegalArgumentException("Invalid external ID")
-                        val type = request["type"]?.jsonPrimitive?.contentOrNull ?: throw IllegalArgumentException("Invalid type")
+                        val repo = request["repo"]?.jsonPrimitive?.contentOrNull
+                            ?: throw IllegalArgumentException("Invalid repo")
+                        val id = request["id"]?.jsonPrimitive?.intOrNull
+                            ?: throw IllegalArgumentException("Invalid external ID")
+                        val type = request["type"]?.jsonPrimitive?.contentOrNull
+                            ?: throw IllegalArgumentException("Invalid type")
                         manager.linkReopend(repo, id, type)
                         call.respond(HttpStatusCode.OK)
                     }
@@ -97,4 +147,13 @@ fun Application.configureRouting(manager: LogManager) {
             }
         }
     }
+}
+
+private suspend fun RoutingContext.getId(): String? {
+    val idParameter = call.parameters["id"]
+    if (idParameter == null || (idParameter.length != 8 && idParameter != "local")) {
+        call.respond(HttpStatusCode.BadRequest, "Missing or invalid ID")
+        return null
+    }
+    return idParameter
 }
